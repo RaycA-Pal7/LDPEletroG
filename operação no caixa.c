8f51340de0ca5d53f8ac920cc4ee35316e83ae3b
RaycA-Pal7 <raycasousa102030@gmail.com>
2024-10-23 16:57:03 -0300

    Add files via upload

---
+++ operação no caixa.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+
+    int numero;
+     printf("informe o numero");
+     scanf("%d",&numero);
+
+     if(numero == 1){
+      printf("operacao saldo");} 
+
+     if(numero == 2){
+        printf("operacao extrato");}
+
+     if(numero == 3){
+        printf("operacao saque");}
+
+       if(numero == 4){
+        printf("operacao sair");}
+      
+
+
+    
+}
