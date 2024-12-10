105f562677ee409864e4cc23a783abe238a03030
RaycA-Pal7 <raycasousa102030@gmail.com>
2024-12-10 10:49:30 -0300

    Add files via upload

---
+++ somaponderada.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+     int x,y,t=1;
+
+    printf("qual o valor de x?");
+    scanf("%d", &x);
+    printf("qual o valor de y?");
+    scanf("%d", &y);
+
+    while(t % x !=0 || t % y !=0){
+        t++; 
+    }
+    printf("2 alarmes");
+    printf("%d", t);
+}
